4f759c6aad3ddfdea50cddb36cc47767b1c7436b
AtsukiSekine <sekine3142@gmail.com>
2025-10-15 15:13:57 +0900

    Address.javaの実装

---
+++ src/main/java/org/example/entity/user/Address.java
@@ -0,0 +1,13 @@
+package org.example.entity.user;
+
+public class Address {
+    private Prefecture prefecture;
+    private String detailAddress;
+
+    // コンストラクタ
+    public Address(Prefecture prefecture, String detailAddress) {
+        this.prefecture = prefecture;
+        this.detailAddress = detailAddress;
+    }
+
+}
