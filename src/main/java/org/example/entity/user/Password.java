de560f3174f87db2eb2405bf25e64224011348b6
AtsukiSekine <sekine3142@gmail.com>
2025-10-15 14:20:21 +0900

    Password.javaの実装

---
+++ src/main/java/org/example/entity/user/Password.java
@@ -0,0 +1,16 @@
+package org.example.entity.user;
+
+public class Password {
+    private String password;
+
+    // 入力規則: 8文字以上で、大文字、小文字、数字、特殊文字をそれぞれ少なくとも1つ含む
+    static final String REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}|\\[\\]:\";'<>?,./]).{8,}$";
+
+    // コンストラクタ
+    public Password(String password) {
+        if (!password.matches(REGEX)) {
+            throw new IllegalArgumentException("入力形式が正しくありません。");
+        }
+        this.password = password;
+    }
+}
